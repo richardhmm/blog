a6cf5947da565f4bc9f880688f3277e4f233c8ad
richardhmm <sunhyx@gmail>
2014-12-05 18:22:30 +0800

    增加评论

--- js/post.js
+++ js/post.js
@@ -22,6 +22,14 @@ $(document).ready(function(){
     };
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
+	
+	// 增加评论
+	window.disqus_shortname = 'richardhmm'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
