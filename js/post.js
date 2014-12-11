46206f548e2fc9499b64de6e9c901e367b35a591
richardhmm <sunhyx@gmail>
2014-12-11 12:33:28 +0800

    增加评论功能

--- js/post.js
+++ js/post.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 	
 	// 增加评论
-	window.disqus_shortname = 'richardhmm'; // required: replace example with your forum shortname
+	window.disqus_shortname = 'richardhmmgithubblog'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
